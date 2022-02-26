b4221af74c8172597d74f85d188198670f24feee
nickbrown33 <nbrown8488@gmail.com>
2022-02-26 15:03:36 -0600

    Create player.sql
    Players for game

---
+++ player.sql
@@ -0,0 +1,12 @@
+--Create table
+CREATE TABLE player (
+  id INT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  codename VARCHAR(30)
+);
+
+--Place first record into table
+INSERT INTO player (id, first_name, last_name, codename)
+VALUES (1, 'Nick', 'Brown', 'NFB');
+VALUES (2, 'Austin', 'Flynn', 'Flynnagin');
