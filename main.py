b52e107553c823274a5b5b6a348b14c410ea32d4
nickbrown33 <nbrown8488@gmail.com>
2022-02-27 17:13:27 -0600

    MidHandler, main update
    Created the middle handler and connected it with main

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from flask import jsonify
+from midhand import connectToHeroku 
 
 app = Flask(__name__)
+mh = connectToHeroku(11, "David", "Hammons", "Davy Gravy")
+mh.addPlayer()
 
 @app.route("/")
 def home():
     return render_template('PlayerScreen.html')
 
-
-
-
-
+def addPlayerThroughMH():
+    pass
